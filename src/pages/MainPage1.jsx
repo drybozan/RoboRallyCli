b9078e56020ec3dae5635021c250f5c63149926d
derya bozan <deryabozan@atmaca.com.tr>
2023-12-13 16:15:02 +0300

    edited ui 2

--- src/pages/MainPage1.jsx
+++ src/pages/MainPage1.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { Button ,Modal} from 'react-bootstrap';
+import { Button, Modal, Form } from 'react-bootstrap';
 import { useNavigate, Link } from "react-router-dom";
 import { toast } from "react-toastify";
 
@@ -13,12 +13,20 @@ export default function MainDashboard() {
 
   const navigate = useNavigate();
 
-  //modal için
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  //yarismacı ekleme modalı için
+  const [showAdd, setShowAdd] = useState(false);
+  const handleShowAddClose = () => setShowAdd(false);
+  const handleShowAddOpen = () => setShowAdd(true);
 
-  const numOfSections = 12; // Kaç parça olacağını belirtin
+  //yarismacı guncelleme modalı için
+  const [showUpdate, setShowUpdate] = useState(false);
+  const handleShowUpdateClose = () => setShowUpdate(false);
+  const handleShowUpdateOpen = () => setShowUpdate(true);
+
+  const [time, setTime] = useState({ minutes: 0, seconds: 0, milliseconds: 0 });
+  const [isRunning, setIsRunning] = useState(false);
+
+  const numOfSections = 20; // Kaç parça olacağını belirtin
 
 
 
@@ -39,41 +47,193 @@ export default function MainDashboard() {
 
   }, []);
 
-  async function handleIconUpdateClick() { }
-  async function handleIconDeleteClick() { }
-  async function addCompetitorClick() {
 
-    setShow(false);
 
-   }
-  async function nextPageClick() { 
-    navigate('/main/page2')
+  async function nextPageClick() {
+
+    const dataToSend = { key: numOfSections }; // Göndermek istediğiniz veri
+
+    navigate('/main/page2', { state: dataToSend });
   }
 
   async function logOutClick() {
-    localStorage.removeItem("username") 
+    localStorage.removeItem("username")
     navigate('/')
   }
 
-  const sections = Array.from({ length: numOfSections }, (_, index) => (
+  // yarismacı düzenleme
+  async function handleIconUpdateClick(index) {
 
-    <div style={{ width: "95%", height: "8%", borderRadius: "50px", backgroundImage: 'linear-gradient(to right, #DB6E1A, #FFDB00)', opacity: 0.8, display: 'flex', alignItems: 'center', marginBottom: "0.5%" }}>
-      <div style={{ flex: "0.3" }}>{index} </div>
-      <div style={{ flex: "0.5" }}>{index} parca 1 </div>
-      <div style={{ flex: "2" }}>{index}  parca 2 </div>
-      <div style={{ flex: "1" }}>{index}  parca 3 </div>
-      <div style={{ flex: "0.5", display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+    console.log("update click index : ")
+    console.log(index)
+    //modal aç
+    handleShowUpdateOpen()
+  }
 
-        <div onClick={() => handleIconUpdateClick(index)} style={{ cursor: 'pointer', marginRight: '5px'}}>
-          <img src={`${process.env.PUBLIC_URL}/updateIcon.png`} alt="Icon update" width="35" height="35" />
-        </div>
-        <div onClick={() => handleIconDeleteClick(index)} style={{ cursor: 'pointer', marginLeft: '5px' }}>
-          <img src={`${process.env.PUBLIC_URL}/deleteIcon.png`} alt="Icon delete" width="35" height="35" />
-        </div>
+  async function updateCompetitorClick() {
 
-      </div>
-    </div>
-  ));
+    //modal kapat
+    handleShowUpdateClose()
+  }
+
+
+  async function handleIconDeleteClick() { }
+
+  async function addCompetitorClick() {
+
+    //modal kapat
+    handleShowAddClose()
+
+  }
+
+  useEffect(() => {
+
+    console.log("main page 1 : ");
+    console.log(localStorage.getItem("running"));
+
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        // Süreyi güncelle
+        setTime((prevTime) => {
+          const newMilliseconds = prevTime.milliseconds + 10;
+
+          // 1000 milisaniye 1 saniyedir
+          if (newMilliseconds === 1000) {
+            return {
+              minutes: prevTime.minutes,
+              seconds: prevTime.seconds + 1,
+              milliseconds: 0,
+            };
+          }
+
+          // 60 saniye 1 dakikadır
+          if (prevTime.seconds === 60) {
+            return {
+              minutes: prevTime.minutes + 1,
+              seconds: 0,
+              milliseconds: 0,
+            };
+          }
+
+          return {
+            ...prevTime,
+            milliseconds: newMilliseconds,
+          };
+        });
+      }, 10); // Her 10 milisaniyede bir güncelle
+    }
+
+    // Temizleme işlemi
+    return () => clearInterval(intervalId);
+  }, [isRunning]);
+
+  const handleStartStopClick = () => {
+    setIsRunning(!isRunning);
+  };
+
+  const formattedTime = `${String(time.minutes).padStart(2, '0')}:${String(time.seconds).padStart(2, '0')}.${String(time.milliseconds).padStart(3, '0')}`;
+
+
+  const sections = [];
+
+  for (let index = 0; index < numOfSections; index++) {
+    if (index >= 0 && index <= 9) {
+      if (index === 0 || index === 1 || index === 2) {
+
+        sections.push(
+
+          // yarışmacıları listeleyen yatay cubuk
+          <div style={{ width: "95%", height: "8%", borderRadius: "50px", backgroundImage: `linear-gradient(to left, rgba(255, 255, 153, 0.5), rgba(220, 50, 5, 0.7))`, display: 'flex', alignItems: 'center', marginBottom: "0.5%", border: "2px solid white" }}>
+
+            {/* sıralama kısmı */}
+            <div style={{ flex: "0.3" }}>
+
+              {/* sıralamanın yazıldığı yuvarlaklar*/}
+              <div style={{ width: "60%", height: "100%", borderRadius: "50%" }}>
+                {index === 0 && (
+
+                  <img src={`${process.env.PUBLIC_URL}/1.png`} alt="Icon gold" width="100" height="100"  style={{ alignSelf: 'flex-start', marginLeft: '-20px' }}/>
+                )}
+                {index === 1 && (
+
+                  <img src={`${process.env.PUBLIC_URL}/2.png`} alt="Icon silver" width="100" height="100"  style={{ alignSelf: 'flex-start', marginLeft: '-20px' }}/>
+
+                )}
+                {index === 2 && (
+
+                  <img src={`${process.env.PUBLIC_URL}/3.png`} alt="Icon bronz" width="100" height="100"  style={{ alignSelf: 'flex-start', marginLeft: '-20px' }}/>
+
+                )}
+
+              </div>
+
+            </div>
+
+            <div style={{ flex: "0.5" , fontWeight: 'bold', fontSize: "30px",color:"white"}}> AĞRI   </div>
+            <div style={{ flex: "2" , fontWeight: 'bold', fontSize: "30px",color:"white"}}> A ORTAOKULU   </div>
+            <div style={{ flex: "1", color: 'white', fontWeight: 'bold', fontSize: "30px" }}>
+
+              {formattedTime}
+
+            </div>
+            <div style={{ flex: "0.5", display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+
+              <div onClick={() => handleIconUpdateClick(index)} style={{ cursor: 'pointer', marginRight: '5px' }}>
+                <img src={`${process.env.PUBLIC_URL}/updateIcon.png`} alt="Icon update" width="35" height="35" />
+              </div>
+              <div onClick={() => handleIconDeleteClick(index)} style={{ cursor: 'pointer', marginLeft: '5px' }}>
+                <img src={`${process.env.PUBLIC_URL}/deleteIcon.png`} alt="Icon delete" width="35" height="35" />
+              </div>
+
+            </div>
+          </div>
+
+        );
+
+      } else {
+
+
+        sections.push(
+
+          // yarışmacıları listeleyen yatay cubuk
+          <div style={{ width: "95%", height: "8%", borderRadius: "50px",  backgroundImage: `linear-gradient(to left, rgba(255, 255, 153, 0.3), rgba(241, 108, 5, 0.3))`, display: 'flex', alignItems: 'center', marginBottom: "0.5%", border: "2px solid white" }}>
+
+            {/* sıralama kısmı */}
+            <div style={{ flex: "0.3"}}>
+
+              {index !== 0 && index !== 1 && index !== 2 && (
+
+                <img src={`${process.env.PUBLIC_URL}/${index+1}.png`} alt={`Icon ${index}`} width="122" height="122"  style={{ alignSelf: 'flex-start', marginLeft: '-55px' }} />
+
+              )}
+            </div>
+
+            <div style={{ flex: "0.5" , fontWeight: 'bold', fontSize: "30px",color:"white"}}>ARDAHAN </div>
+            <div style={{ flex: "2" , fontWeight: 'bold', fontSize: "30px",color:"white"}}> ARDAHAN LİSE  </div>
+            <div style={{ flex: "1", color: 'white', fontWeight: 'bold', fontSize: "30px" }}>
+
+              {formattedTime}
+
+            </div>
+            <div style={{ flex: "0.5", display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+
+              <div onClick={() => handleIconUpdateClick(index)} style={{ cursor: 'pointer', marginRight: '5px' }}>
+                <img src={`${process.env.PUBLIC_URL}/updateIcon.png`} alt="Icon update" width="35" height="35" />
+              </div>
+              <div onClick={() => handleIconDeleteClick(index)} style={{ cursor: 'pointer', marginLeft: '5px' }}>
+                <img src={`${process.env.PUBLIC_URL}/deleteIcon.png`} alt="Icon delete" width="35" height="35" />
+              </div>
+
+            </div>
+          </div>
+
+        );
+      }
+    }
+  }
+
 
 
   return (
@@ -83,27 +243,29 @@ export default function MainDashboard() {
 
         <img src={`${process.env.PUBLIC_URL}/homeLogo.png`} alt="sunnyTeknolojiLogo" style={{ width: '25%', height: '15vh', position: 'fixed', top: '2%', left: '3%' }} />
 
-        <div onClick={handleShow} style={{ cursor: 'pointer' }}>
+        <div onClick={handleShowAddOpen} style={{ cursor: 'pointer' }}>
           <img src={`${process.env.PUBLIC_URL}/addCompetitor.png`} alt="add competitor" style={{ width: '15%', height: '8vh', position: 'fixed', top: '4.5%', right: '13%' }} />
         </div>
 
 
         <div onClick={() => logOutClick()} style={{ cursor: 'pointer' }}>
-          <img src={`${process.env.PUBLIC_URL}/logout.png`} alt="logout" style={{ width: '6%', height: '7vh', position: 'fixed', top: '5.5%', right: '7%' }} />
+          <img src={`${process.env.PUBLIC_URL}/logout.png`} alt="logout" style={{ width: '7%', height: '7vh', position: 'fixed', top: '5.5%', right: '6%' }} />
         </div>
 
       </div>
 
 
-      <div style={{ width: "95%", height: "75%", display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: "3%" }}>
+      <div style={{ width: "95%", height: "77%", display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: "3%" }}>
 
         {/* tablo başlıkları  */}
         <div style={{ width: "95%", height: "7%", display: 'flex', alignItems: 'center', marginBottom: "0.5%", color: '#fff', fontWeight: 'bold', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.5)' }}>
           <div style={{ flex: "0.3" }}>SIRALAMA </div>
           <div style={{ flex: "0.5" }}>ŞEHİR </div>
-          <div style={{ flex: "2" }}> OKUL </div>
+          <div style={{ flex: "2" }}> YARIŞMACI </div>
           <div style={{ flex: "1" }}> SÜRE </div>
-          <div style={{ flex: "0.5" }}></div>
+          <div style={{ flex: "0.5" }} onClick={() => handleStartStopClick()}>
+            <img src={`${process.env.PUBLIC_URL}/start.png`} alt="start" style={{ width: '5vw', height: '6vh' }} />
+          </div>
         </div>
 
 
@@ -116,21 +278,90 @@ export default function MainDashboard() {
         <img src={`${process.env.PUBLIC_URL}/firstPage.png`} alt="first page" style={{ width: '6%', height: '7vh', position: 'fixed', bottom: '2%', right: '7%' }} />
       </div>
 
+      {/* YARIŞMACI EKLEME MODAL */}
+
       <>
 
-      <Modal show={show} onHide={handleClose} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>YARIŞMACI EKLE</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-        
-          <Button variant="success" onClick={addCompetitorClick}>
-            Kaydet
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+        <Modal show={showAdd} onHide={handleShowAddClose} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>YARIŞMACI EKLE</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>ŞEHİR</Form.Label>
+                <Form.Select aria-label="Default select example">
+                  <option>Şehir seçiniz.</option>
+                  <option value="1">Ağrı</option>
+                  <option value="2">Ardahan</option>
+                  <option value="3">Iğdır</option>
+                  <option value="4">Kars</option>
+                </Form.Select>
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>YARIŞMACI İSMİ</Form.Label>
+                <Form.Control as="textarea" rows={2} />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="outline-success" onClick={addCompetitorClick}>
+              Kaydet
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
+
+        {/* YARIŞMACI DÜZENLEME MODAL */}
+
+
+        <Modal show={showUpdate} centered>
+          <Modal.Header >
+            <Modal.Title>YARIŞMACI DÜZENLE</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>ŞEHİR</Form.Label>
+                <Form.Select aria-label="Default select example">
+                  <option>Şehir seçiniz.</option>
+                  <option value="1">Ağrı</option>
+                  <option value="2">Ardahan</option>
+                  <option value="3">Iğdır</option>
+                  <option value="4">Kars</option>
+                </Form.Select>
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>YARIŞMACI İSMİ</Form.Label>
+                <Form.Control as="textarea" rows={2} />
+              </Form.Group>
+
+              <Form.Check // prettier-ignore
+                type="switch"
+                id="custom-switch"
+                label="Yarışmacıyı devre dışı bırak."
+              />
+            </Form>
+          </Modal.Body>
+
+          <Modal.Footer>
+
+            <Button variant="outline-danger" onClick={handleShowUpdateClose} >
+              Vazgeç
+            </Button>
+
+            <Button variant="outline-success" onClick={updateCompetitorClick}>
+              Kaydet
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
 
     </div>
   )
--- src/pages/MainPage2.jsx
+++ src/pages/MainPage2.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { Button, Modal } from 'react-bootstrap';
-import { useNavigate, Link } from "react-router-dom";
+import { Button, Modal, Form } from 'react-bootstrap';
+import { useNavigate, useLocation } from "react-router-dom";
 import { toast } from "react-toastify";
 
 
@@ -12,8 +12,25 @@ export default function MainDashboard() {
     const [screenHeight, setScreenHeight] = useState(window.innerHeight); // tarayıcı penceresinin mevcut yüksekliği
 
     const navigate = useNavigate();
+    const location = useLocation();
 
-    const numOfSections = 12; // Kaç parça olacağını belirtin
+
+    //yarismacı guncelleme modalı için
+    const [showUpdate, setShowUpdate] = useState(false);
+    const handleShowUpdateClose = () => setShowUpdate(false);
+    const handleShowUpdateOpen = () => setShowUpdate(true);
+
+
+
+    const [time, setTime] = useState({ minutes: 0, seconds: 0, milliseconds: 0 });
+    const [isRunning, setIsRunning] = useState(false);
+
+    const receivedData = location.state;
+    // console.log("receivedData")
+    // console.log(receivedData)
+
+    const numOfSections = receivedData.key; // Kaç parça olacağını belirtin
+
 
     useEffect(() => {
         // Function to update screen dimensions on resize
@@ -32,9 +49,6 @@ export default function MainDashboard() {
 
     }, []);
 
-    async function handleIconUpdateClick() { }
-    async function handleIconDeleteClick() { }
-
     async function previousPageClick() {
         navigate('/main')
     }
@@ -44,25 +58,111 @@ export default function MainDashboard() {
         navigate('/')
     }
 
-    const sections = Array.from({ length: numOfSections }, (_, index) => (
 
-        <div style={{ width: "95%", height: "8%", borderRadius: "50px", backgroundImage: 'linear-gradient(to right, #DB6E1A, #FFDB00)', opacity: 0.8, display: 'flex', alignItems: 'center', marginBottom: "0.5%" }}>
-            <div style={{ flex: "0.3" }}>{index} </div>
-            <div style={{ flex: "0.5" }}>{index} parca 1 </div>
-            <div style={{ flex: "2" }}>{index}  parca 2 </div>
-            <div style={{ flex: "1" }}>{index}  parca 3 </div>
-            <div style={{ flex: "0.5", display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+    // yarismacı düzenleme
+    async function handleIconUpdateClick(index) {
 
-                <div onClick={() => handleIconUpdateClick(index)} style={{ cursor: 'pointer', marginRight: '5px' }}>
-                    <img src={`${process.env.PUBLIC_URL}/updateIcon.png`} alt="Icon update" width="35" height="35" />
+        console.log("update click index : ")
+        console.log(index)
+        //modal aç
+        handleShowUpdateOpen()
+    }
+
+    async function updateCompetitorClick() {
+
+        //modal kapat
+        handleShowUpdateClose()
+    }
+
+
+    async function handleIconDeleteClick() { }
+
+
+    useEffect(() => {
+
+        let intervalId;
+
+        if (isRunning) {
+            intervalId = setInterval(() => {
+                // Süreyi güncelle
+                setTime((prevTime) => {
+                    const newMilliseconds = prevTime.milliseconds + 10;
+
+                    // 1000 milisaniye 1 saniyedir
+                    if (newMilliseconds === 1000) {
+                        return {
+                            minutes: prevTime.minutes,
+                            seconds: prevTime.seconds + 1,
+                            milliseconds: 0,
+                        };
+                    }
+
+                    // 60 saniye 1 dakikadır
+                    if (prevTime.seconds === 60) {
+                        return {
+                            minutes: prevTime.minutes + 1,
+                            seconds: 0,
+                            milliseconds: 0,
+                        };
+                    }
+
+                    return {
+                        ...prevTime,
+                        milliseconds: newMilliseconds,
+                    };
+                });
+            }, 10); // Her 10 milisaniyede bir güncelle
+        }
+
+        // Temizleme işlemi
+        return () => clearInterval(intervalId);
+    }, [isRunning]);
+
+    const formattedTime = `${String(time.minutes).padStart(2, '0')}:${String(time.seconds).padStart(2, '0')}.${String(time.milliseconds).padStart(3, '0')}`;
+
+
+    const sections = [];
+
+    for (let index = 0; index < numOfSections; index++) {
+        if (index >= 10 && index <= 19) {
+            sections.push(
+
+                <div style={{ width: "95%", height: "8%", borderRadius: "50px",  backgroundImage: `linear-gradient(to left, rgba(255, 255, 153, 0.3), rgba(241, 108, 5, 0.3))`, display: 'flex', alignItems: 'center', marginBottom: "0.5%", border: "2px solid white" }}>
+
+                    {/* sıralama kısmı */}
+                    <div style={{ flex: "0.3" }}>
+
+                        <img src={`${process.env.PUBLIC_URL}/${index + 1}.png`} alt={`Icon ${index}`} width="122" height="122" style={{ alignSelf: 'flex-start', marginLeft: '-55px' }} />
+
+
+                    </div>
+
+                    <div style={{ flex: "0.5" ,color: '#000', fontWeight: 'bold', fontSize: "30px" }}>
+                        KARS  </div>
+                    <div style={{ flex: "2", fontWeight: 'bold', fontSize: "30px" }}>
+                        
+                        KARS ORTAOKUL </div>
+
+                    <div style={{ flex: "1", color: '#000', fontWeight: 'bold', fontSize: "30px" }}>
+
+                        {formattedTime}
+
+                    </div>
+                    <div style={{ flex: "0.5", display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+
+                        <div onClick={() => handleIconUpdateClick(index)} style={{ cursor: 'pointer', marginRight: '5px' }}>
+                            <img src={`${process.env.PUBLIC_URL}/updateIcon.png`} alt="Icon update" width="35" height="35" />
+                        </div>
+                        <div onClick={() => handleIconDeleteClick(index)} style={{ cursor: 'pointer', marginLeft: '5px' }}>
+                            <img src={`${process.env.PUBLIC_URL}/deleteIcon.png`} alt="Icon delete" width="35" height="35" />
+                        </div>
+
+                    </div>
                 </div>
-                <div onClick={() => handleIconDeleteClick(index)} style={{ cursor: 'pointer', marginLeft: '5px' }}>
-                    <img src={`${process.env.PUBLIC_URL}/deleteIcon.png`} alt="Icon delete" width="35" height="35" />
-                </div>
 
-            </div>
-        </div>
-    ));
+            );
+        }
+    }
 
 
     return (
@@ -71,19 +171,19 @@ export default function MainDashboard() {
             <div style={{ display: 'flex', justifyContent: 'center' }}>
 
                 <div onClick={() => logOutClick()} style={{ cursor: 'pointer' }}>
-                    <img src={`${process.env.PUBLIC_URL}/logout.png`} alt="logout" style={{ width: '6%', height: '7vh', position: 'fixed', top: '5.5%', right: '7%' }} />
+                    <img src={`${process.env.PUBLIC_URL}/logout.png`} alt="logout" style={{ width: '7%', height: '7vh', position: 'fixed', top: '5.5%', right: '6%' }} />
                 </div>
 
             </div>
 
 
-            <div style={{ width: "95%", height: "75%", display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: "0.1%" }}>
+            <div style={{ width: "95%", height: "77%", display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: "0.1%" }}>
 
                 {/* tablo başlıkları  */}
                 <div style={{ width: "95%", height: "7%", display: 'flex', alignItems: 'center', marginBottom: "0.5%", color: '#fff', fontWeight: 'bold', boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.5)' }}>
                     <div style={{ flex: "0.3" }}>SIRALAMA </div>
                     <div style={{ flex: "0.5" }}>ŞEHİR </div>
-                    <div style={{ flex: "2" }}> OKUL </div>
+                    <div style={{ flex: "2" }}> YARIŞMACI </div>
                     <div style={{ flex: "1" }}> SÜRE </div>
                     <div style={{ flex: "0.5" }}></div>
                 </div>
@@ -96,13 +196,60 @@ export default function MainDashboard() {
 
             <div style={{ display: 'flex', justifyContent: 'center' }}>
 
-                <img src={`${process.env.PUBLIC_URL}/homeLogo2.png`} alt="sunnyTeknolojiLogo" style={{ width: '20%', height: '13vh', position: 'fixed', bottom: '0.1%', left: '37%' }} />
+                <img src={`${process.env.PUBLIC_URL}/homeLogo2.png`} alt="sunnyTeknolojiLogo" style={{ width: '18%', height: '13vh', position: 'fixed', bottom: '0.1%', left: '37%' }} />
             </div>
 
             <div onClick={() => previousPageClick()} style={{ cursor: 'pointer' }}>
                 <img src={`${process.env.PUBLIC_URL}/secondPage.png`} alt="second page" style={{ width: '6%', height: '7vh', position: 'fixed', bottom: '2%', right: '7%' }} />
             </div>
 
+            {/* YARIŞMACI DÜZENLEME MODAL */}
+
+            <>
+                <Modal show={showUpdate} centered>
+                    <Modal.Header >
+                        <Modal.Title>YARIŞMACI DÜZENLE</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label>ŞEHİR</Form.Label>
+                                <Form.Select aria-label="Default select example">
+                                    <option>Şehir seçiniz.</option>
+                                    <option value="1">Ağrı</option>
+                                    <option value="2">Ardahan</option>
+                                    <option value="3">Iğdır</option>
+                                    <option value="4">Kars</option>
+                                </Form.Select>
+                            </Form.Group>
+                            <Form.Group
+                                className="mb-3"
+                                controlId="exampleForm.ControlTextarea1"
+                            >
+                                <Form.Label>YARIŞMACI İSMİ</Form.Label>
+                                <Form.Control as="textarea" rows={2} />
+                            </Form.Group>
+
+                            <Form.Check // prettier-ignore
+                                type="switch"
+                                id="custom-switch"
+                                label="Yarışmacıyı devre dışı bırak."
+                            />
+                        </Form>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+
+                        <Button variant="outline-danger" onClick={handleShowUpdateClose} >
+                            Vazgeç
+                        </Button>
+
+                        <Button variant="outline-success" onClick={updateCompetitorClick}>
+                            Kaydet
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
 
 
         </div>
